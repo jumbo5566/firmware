6db3a5eb33f4afda8ed76d7bc44bad7e433a3fa0
jumbo5566 <jumbo5566@qq.com>
2024-06-27 18:48:23 +0800

    Update variant.h
    fix slow clock

--- variants/rpipico-slowclock/variant.h
+++ variants/rpipico-slowclock/variant.h
@@ -1,9 +1,9 @@
 #define ARDUINO_ARCH_AVR
 
 // Build with slow system clock enabled to reduce power consumption.
-#define RP2040_SLOW_CLOCK
+//#define RP2040_SLOW_CLOCK
 
-#ifdef RP2040_SLOW_CLOCK
+//#ifdef RP2040_SLOW_CLOCK
 // Redefine UART1 serial log output to avoid collision with UART0 for GPS.
 #define SERIAL2_TX 6
 #define SERIAL2_RX 7
@@ -11,15 +11,13 @@
 #define log_e(...) Serial2.printf(__VA_ARGS__)
 #define log_i(...) Serial2.printf(__VA_ARGS__)
 #define log_d(...) Serial2.printf(__VA_ARGS__)
-#endif
+//#endif
 
 // Expecting the Waveshare Pico GPS hat
 #define HAS_GPS 1
 
 // Enable OLED Screen
 #define HAS_SCREEN 1
-//#define USE_SH1106 1
-//#define RESET_OLED 13
 
 // Redefine I2C0 pins to avoid collision with UART1/Serial2.
 #define I2C_SDA 8
@@ -28,27 +26,16 @@
 // Redefine Waveshare UPS-A/B I2C_1 pins:
 #define I2C_SDA1 18
 #define I2C_SCL1 19
-// Waveshare UPS-A/B uses a 0.01 Ohm shunt for the INA219 sensor
-//#define INA219_MULTIPLIER 10.0f
 
 // Waveshare Pico GPS L76B pins:
 #define GPS_RX_PIN 1
 #define GPS_TX_PIN 0
 
-// Wakeup from backup mode
-// #define PIN_GPS_FORCE_ON 14
-// No GPS reset available
-//#undef PIN_GPS_RESET
 /*
  * For PPS output the resistor R20 needs to be populated with 0 Ohm
  * on the Waveshare Pico GPS board.
  */
 //#define PIN_GPS_PPS 16
-/*
- * For standby mode switching the resistor R18 needs to be populated
- * with 0 Ohm on the Waveshare Pico GPS board.
- */
-//#define PIN_GPS_STANDBY 17
 
 #define BUTTON_PIN 17
 #define EXT_NOTIFY_OUT 22
