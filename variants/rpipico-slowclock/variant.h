8befb778f6ac79b73a62dd91f34390c1d5d09b87
jumbo5566 <jumbo5566@qq.com>
2024-06-28 11:15:25 +0800

    Update variant.h
    update slowclock

--- variants/rpipico-slowclock/variant.h
+++ variants/rpipico-slowclock/variant.h
@@ -1,63 +1,66 @@
+// #define RADIOLIB_CUSTOM_ARDUINO 1
+// #define RADIOLIB_TONE_UNSUPPORTED 1
+// #define RADIOLIB_SOFTWARE_SERIAL_UNSUPPORTED 1
+
 #define ARDUINO_ARCH_AVR
 
 // Build with slow system clock enabled to reduce power consumption.
 #define RP2040_SLOW_CLOCK
 
+#ifdef RP2040_SLOW_CLOCK
 // Redefine UART1 serial log output to avoid collision with UART0 for GPS.
-#define SERIAL2_TX 6
-#define SERIAL2_RX 7
+
+#define SERIAL2_RX 8
+#define SERIAL2_TX 9
+
+#define PIN_SERIAL1_RX 1
+#define PIN_SERIAL1_TX 0
 // Reroute log output in SensorLib when USB is not available
 #define log_e(...) Serial2.printf(__VA_ARGS__)
 #define log_i(...) Serial2.printf(__VA_ARGS__)
 #define log_d(...) Serial2.printf(__VA_ARGS__)
+#endif
 
-// Expecting the Waveshare Pico GPS hat
+#define HAS_SCREEN 1
 #define HAS_GPS 1
 
-// Enable OLED Screen
-#define HAS_SCREEN 1
-
-// Redefine I2C0 pins to avoid collision with UART1/Serial2.
-//#define I2C_SDA 8
-//#define I2C_SCL 9
-
-// Redefine Waveshare UPS-A/B I2C_1 pins:
+//#define I2C_SDA 4
+//#define I2C_SCL 5
 #define I2C_SDA1 18
 #define I2C_SCL1 19
 
-// Waveshare Pico GPS L76B pins:
-#define GPS_RX_PIN 1
-#define GPS_TX_PIN 0
+#define GPS_RX_PIN PIN_SERIAL1_RX
+#define GPS_TX_PIN PIN_SERIAL1_TX
 
-/*
- * For PPS output the resistor R20 needs to be populated with 0 Ohm
- * on the Waveshare Pico GPS board.
- */
 //#define PIN_GPS_PPS 16
+//#define PIN_GPS_STANDBY 17
+//#define GPS_UBLOX
+#define GPS_RX_PIN PIN_SERIAL1_RX
+#define GPS_TX_PIN PIN_SERIAL1_TX
+//#define GPS_DEBUG
 
-#define BUTTON_PIN 17
 #define EXT_NOTIFY_OUT 22
+#define BUTTON_PIN 17
+#define BUTTON_NEED_PULLUP
 #define LED_PIN PIN_LED
 
 #define BATTERY_PIN 26
 // ratio of voltage divider = 3.0 (R17=200k, R18=100k)
 #define ADC_MULTIPLIER 3.1 // 3.0 + a bit for being optimistic
 #define BATTERY_SENSE_RESOLUTION_BITS ADC_RESOLUTION
-
 //#define USE_LLCC68
 //#define USE_RF95 // RFM95/SX127x
 //#define USE_SX1280
 //#define USE_SX1262
 #define USE_SX1268
-
 #undef LORA_SCK
 #undef LORA_MISO
 #undef LORA_MOSI
 #undef LORA_CS
 
 #define LORA_SCK 10
-#define LORA_MISO 12
 #define LORA_MOSI 11
+#define LORA_MISO 12
 #define LORA_CS 13
 
 #define LORA_DIO0 RADIOLIB_NC
@@ -73,5 +76,4 @@
 #define SX126X_RESET LORA_RESET
 #define SX126X_DIO2_AS_RF_SWITCH
 //#define SX126X_DIO3_TCXO_VOLTAGE 1.8
-#define LORA_DIO3 RADIOLIB_NC
 //#endif
