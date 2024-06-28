1cd69c37583c2bb817766ff83d4817031da1fe93
jumbo5566 <jumbo5566@qq.com>
2024-06-28 14:03:41 +0800

    Update GPS.cpp
    set rx buf 256

--- src/gps/GPS.cpp
+++ src/gps/GPS.cpp
@@ -1380,7 +1380,7 @@ GPS *GPS::createGps()
         _serial_gps->setRxBufferSize(SERIAL_BUFFER_SIZE); // the default is 256
 #endif
 #ifdef RPI_PICO
-        _serial_gps->setFIFOSize(128);
+        _serial_gps->setRxBufferSize(256); // the default is 256
 #endif
 //  ESP32 has a special set of parameters vs other arduino ports
 #if defined(ARCH_ESP32)
